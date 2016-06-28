6a7a045617d2961fb11d34fb88ce1d2c28cd5b17
xiaomo <83387856@qq.com>
2016-06-28 17:18:48 +0800

    后台路由

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,53 @@
 /* 
  * Angular
  */
-import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ROUTER_PROVIDERS } from '@angular/router';
+import {Component} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { BasicComponent} from './component/Demo/Basic';
+import { APP_ROUTER_PROVIDERS} from './app.routes';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { BasicComponent } from './component/Demo/Basic';
+
 
 @Component({
     selector: 'aries',
     directives: [
+        ROUTER_DIRECTIVES,
         BasicComponent
     ],
     template: `
-        <div>
-    <table class="table table-bordered table-striped table-hover">
-        <tbody>
-        <tr>
-            <td>基本组件</td>
-            <td>
-                <basic></basic>
-            </td>
-        </tr>
-        <tr>
-        </tbody>
-    </table>
-
+           <div class="container">
+    <div class="row">
+<!-- 左侧导航 -->
+        <div class="col-md-2">
+            <ul class="nav nav-list">
+                <li class="list-group-item"><a [routerLink]="['']">后台首页<i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['admin']">权限管理 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['user']">用户管理 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['tag']">标签管理 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['changeLog']">更新日志 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['goodBlog']">优秀博客 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['goodPlugin']">优秀插件 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['friendLink']">友情链接 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+                <li class="list-group-item"><a [routerLink]="['systemSet']">系统设置 <i class="glyphicon glyphicon-chevron-right"></i></a></li>
+            </ul>
+        </div>
+        <!-- 右侧内容 -->
+        <div class="col-md-10">
+            <router-outlet></router-outlet>
+        </div>
+    </div>
 </div>
     `
 })
-class Aries {
+
+export class Aries {
+
 }
 
-bootstrap(Aries,[
-    ROUTER_PROVIDERS,
+bootstrap(Aries, [
+    APP_ROUTER_PROVIDERS,
     HTTP_PROVIDERS
-]);
+]).catch((err:any) => {
+    console.log(err);
+});
