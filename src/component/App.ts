db938966196db80238236edd9106e727ccfdfd69
xiaomo <83387856@qq.com>
2016-07-01 13:55:35 +0800

    改改改

---
+++ src/component/App.ts
@@ -0,0 +1,17 @@
+/* 
+ * Angular
+ */
+import {Component} from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+    selector: 'taurus',
+    directives: [
+        ROUTER_DIRECTIVES,
+    ],
+    template: require('./app.html')
+})
+
+export class AppComponent {
+
+}
