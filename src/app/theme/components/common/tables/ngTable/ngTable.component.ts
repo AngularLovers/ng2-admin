da2449ee03e6e736a785b61b5e433d344fc9e301
xiaomo <83387856@qq.com>
2016-07-13 15:15:51 +0800

    格式化代码

--- src/app/theme/components/common/tables/ngTable/ngTable.component.ts
+++ src/app/theme/components/common/tables/ngTable/ngTable.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CORE_DIRECTIVES, NgClass } from '@angular/common';
 import { NgTableSortingDirective } from './ngTable-sorting.directive';
 
@@ -12,50 +12,50 @@ export class NgTableComponent {
   // Table values
   @Input() public rows:Array<any> = [];
   @Input() public config:any = {};
-
+  
   // Outputs (Events)
   @Output() public tableChanged:EventEmitter<any> = new EventEmitter();
-
+  
   @Input()
   public set columns(values:Array<any>) {
     values.forEach((value:any) => {
       let column = this._columns.find((col:any) => col.name === value.name);
-      if (column) {
+      if(column) {
         Object.assign(column, value);
       }
-      if (!column) {
+      if(!column) {
         this._columns.push(value);
       }
     });
   }
-
+  
   public get columns():Array<any> {
     return this._columns;
   }
-
+  
   public get configColumns():any {
     let sortColumns:Array<any> = [];
-
+    
     this.columns.forEach((column:any) => {
-      if (column.sort) {
+      if(column.sort) {
         sortColumns.push(column);
       }
     });
-
+    
     return {columns: sortColumns};
   }
-
+  
   private _columns:Array<any> = [];
-
+  
   public onChangeTable(column:any):void {
     this._columns.forEach((col:any) => {
-      if (col.name !== column.name && col.sort !== false) {
+      if(col.name !== column.name && col.sort !== false) {
         col.sort = '';
       }
     });
     this.tableChanged.emit({sorting: this.configColumns});
   }
-
+  
   public getData(row:any, propertyName:string):string {
     return propertyName.split('.').reduce((prev:any, curr:string) => prev[curr], row);
   }
