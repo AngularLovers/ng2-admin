5136604b4ae5522c25057533c112e25767f04135
xiaomo <83387856@qq.com>
2016-07-23 09:28:38 +0800

    修复BUG

--- src/app/theme/components/common/modals/modal.component.ts
+++ src/app/theme/components/common/modals/modal.component.ts
@@ -1,15 +1,15 @@
-import { Component,OnInit } from '@angular/core';
-import {MODAL_DIRECTVES, BS_VIEW_PROVIDERS} from 'ng2-bootstrap/ng2-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { MODAL_DIRECTIVES, BS_VIEW_PROVIDERS } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'common-modals',
-  directives: [MODAL_DIRECTVES],
-  viewProviders:[BS_VIEW_PROVIDERS],
+  directives: [MODAL_DIRECTIVES],
+  viewProviders: [BS_VIEW_PROVIDERS],
   template: require('./modals.html'),
-  styles:[require('./modal.scss')],
+  styles: [require('./modal.scss')],
 })
 
-export class CommonModalComponent implements OnInit{
+export class CommonModalComponent implements OnInit {
 
   title:string;
 
