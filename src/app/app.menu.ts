47503a4b9d4e335bc2b1097c6f395838ded2eb68
xiaomo <83387856@qq.com>
2016-07-27 17:00:23 +0800

    规范化类

--- src/app/app.menu.ts
+++ src/app/app.menu.ts
@@ -1,9 +1,19 @@
-export interface Menu {
+export class Menu {
   title:string;
   component:string;
-  icon?:string;
-  selected?:boolean;
-  expanded?:boolean;
-  order?:number;
-  subMenu?:Array<Object>;
+  icon:string;
+  selected:boolean;
+  expanded:boolean;
+  order:number;
+  subMenu:Array<Menu>;
+
+  constructor(title:string, component?:string, icon?:string, selected?:boolean, expanded?:boolean, order?:number, subMenu?:Array<Menu>) {
+    this.title = title;
+    this.component = component || '';
+    this.icon = icon || '';
+    this.selected = selected || false;
+    this.expanded = expanded || false;
+    this.order = order || 0;
+    this.subMenu = subMenu || [];
+  }
 }
