9dbbc7c2c8ef0098210dd7cb7cbb164a2e5b09b7
xiaomo <83387856@qq.com>
2016-07-28 11:25:17 +0800

    添加样式

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -6,7 +6,8 @@ import { CommonPaginationComponent } from './../../theme/components/index';
   moduleId: module.id,
   selector: 'test',
   directives:[CommonPaginationComponent],
-  template: require( './test.html')
+  template: require( './test.html'),
+  styles: [require( './test.scss')]
 })
 export class Test implements OnInit {
   constructor() {
